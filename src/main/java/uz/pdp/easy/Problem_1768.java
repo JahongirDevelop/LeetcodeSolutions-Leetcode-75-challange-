cd546a99c491f0b7a8fac32b4454f26f4956c4aa
Jahongir <sjahongir22@gmail.com>
2024-02-02 21:54:04 +0500

    1768 problem

---
+++ src/main/java/uz/pdp/easy/Problem_1768.java
@@ -0,0 +1,26 @@
+package uz.pdp.easy;
+
+public class Problem_1768 {
+    /*
+      Paths :
+      https://leetcode.com/problems/merge-strings-alternately
+      */
+
+    public static void main(String[] args) {
+        String word1 = "abc", word2 = "pqr";
+        System.out.println(mergeAlternately(word1, word2));
+    }
+
+    public static String mergeAlternately(String word1, String word2) {
+        int n = Math.min(word1.length(), word2.length());
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < n; ++i) {
+            sb.append(word1.charAt(i));
+            sb.append(word2.charAt(i));
+        }
+
+        return sb.append(word1.substring(n)).append(word2.substring(n)).toString();
+
+    }
+}
