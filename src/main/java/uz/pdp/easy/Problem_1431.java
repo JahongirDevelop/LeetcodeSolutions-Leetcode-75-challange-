10581c7ed6461c4dc546831796da876f096dcb9f
Jahongir <sjahongir22@gmail.com>
2024-02-02 22:53:12 +0500

    a lot of problems

---
+++ src/main/java/uz/pdp/easy/Problem_1431.java
@@ -0,0 +1,34 @@
+package uz.pdp.easy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Problem_1431 {
+    /*
+      Paths :
+      https://leetcode.com/problems/kids-with-the-greatest-number-of-candies
+      */
+
+    public static void main(String[] args) {
+        int candies[] = {2,3,5,1,3}, extraCandies = 3;
+        kidsWithCandies(candies, extraCandies);
+    }
+
+    public static List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        List<Boolean> result = new ArrayList<>();
+        int max = 0;
+        for (int i = 0; i < candies.length; i++) {
+            if (candies[i] > max) {
+                max = candies[i];
+            }
+        }
+        for (int i = 0; i < candies.length; i++) {
+            if (candies[i] + extraCandies >= max) {
+                result.add(true);
+            } else {
+                result.add(false);
+            }
+        }
+        return result;
+    }
+}
