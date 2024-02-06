d0e0349a4321d7690e745d2f4ef0406239b262cb
Jahongir <sjahongir22@gmail.com>
2024-02-06 21:40:05 +0500

    for easy

---
+++ src/main/java/soqqa/uz/easy/Problem_206.java
@@ -0,0 +1,26 @@
+package soqqa.uz.easy;
+
+public class Problem_206 {
+    /*
+      Path :
+      https://leetcode.com/problems/reverse-linked-list
+      */
+
+    public ListNode reverseList(ListNode head) {
+        if(head==null || head.next== null){
+            return head;
+        }
+        ListNode newHead= reverseList(head.next);
+        head.next.next= head;
+        head.next= null;
+        return newHead;
+    }
+
+     static class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next;}
+    }
+}
