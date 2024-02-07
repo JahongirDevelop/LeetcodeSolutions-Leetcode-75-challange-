45a2371ff873de9b8438414460576e7d001fbc0b
Jahongir <sjahongir22@gmail.com>
2024-02-07 17:33:07 +0500

    for easy

---
+++ src/main/java/soqqa/uz/easy/Problem_700.java
@@ -0,0 +1,32 @@
+package soqqa.uz.easy;
+
+public class Problem_700 {
+    /*
+      Path :
+      https://leetcode.com/problems/first-unique-character-in-a-string
+      */
+
+    public TreeNode searchBST(TreeNode root, int val) {
+        if(root == null)  return null;
+        if(root.val == val) return root;
+
+        if(val < root.val){
+            return searchBST(root.left, val);
+        }else {
+            return searchBST(root.right, val);
+        }
+    }
+
+    static class TreeNode {
+     int val;
+     TreeNode left;
+     TreeNode right;
+     TreeNode() {}
+     TreeNode(int val) { this.val = val; }
+     TreeNode(int val, TreeNode left, TreeNode right) {
+         this.val = val;
+         this.left = left;
+         this.right = right;
+     }
+ }
+}
