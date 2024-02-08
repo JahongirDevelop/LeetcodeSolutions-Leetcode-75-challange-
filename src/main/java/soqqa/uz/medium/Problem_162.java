dd9b5bd36314eba5d1ed528cfa68afa49f0e6f6e
Jahongir <sjahongir22@gmail.com>
2024-02-08 20:00:48 +0500

    for medium

--- src/main/java/soqqa/uz/medium/Problem_162.java
+++ src/main/java/soqqa/uz/medium/Problem_162.java
@@ -7,7 +7,8 @@ public class Problem_162 {
     */
 
     public static void main(String[] args) {
-
+        int[] nums = {1,2,3,1};
+        System.out.println(findPeakElement(nums));
     }
 
     public static int findPeakElement(int[] nums) {
