43ee5881bd42da7dd11ed5f2270ab3382c2ece1d
Jahongir <sjahongir22@gmail.com>
2024-02-08 20:09:43 +0500

    for medium

--- src/main/java/soqqa/uz/medium/Problem_2300.java
+++ src/main/java/soqqa/uz/medium/Problem_2300.java
@@ -1,8 +1,41 @@
 package soqqa.uz.medium;
 
+import java.util.Arrays;
+
 public class Problem_2300 {
      /*
       Path :
       https://leetcode.com/problems/successful-pairs-of-spells-and-potions
      */
+
+    public static void main(String[] args) {
+        int[] spells = {5,1,3};
+        int[] potions = {1,2,3,4,5};
+        long success = 7;
+        System.out.println(Arrays.toString(successfulPairs(spells, potions, success)));
+
+    }
+
+    public static int[] successfulPairs(int[] spells, int[] potions, long success) {
+        int n = spells.length;
+        int m = potions.length;
+        int[] pairs = new int[n];
+        Arrays.sort(potions);
+        for (int i = 0; i < n; i++) {
+            int spell = spells[i];
+            int left = 0;
+            int right = m - 1;
+            while (left <= right) {
+                int mid = left + (right - left) / 2;
+                long product = (long) spell * potions[mid];
+                if (product >= success) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            }
+            pairs[i] = m - left;
+        }
+        return pairs;
+    }
 }
