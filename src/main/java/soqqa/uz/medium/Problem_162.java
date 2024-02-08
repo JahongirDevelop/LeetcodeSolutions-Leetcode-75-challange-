1f986864fbce9393453b834cca9d7a9585a09dc7
Jahongir <sjahongir22@gmail.com>
2024-02-08 19:59:19 +0500

    for medium

--- src/main/java/soqqa/uz/medium/Problem_162.java
+++ src/main/java/soqqa/uz/medium/Problem_162.java
@@ -10,5 +10,24 @@ public class Problem_162 {
 
     }
 
+    public static int findPeakElement(int[] nums) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] > nums[mid + 1]) {
+                // The peak element is in the left half
+                right = mid;
+            } else {
+                // The peak element is in the right half
+                left = mid + 1;
+            }
+        }
+
+        return left;
+    }
+
 
 }
